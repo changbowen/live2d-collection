dfa6dd6e7aa14996b99080e1f69d979d0adf75b9
Carl Chang <changbowen@msn.cn>
2020-01-11 19:52:36 +0800

    add HyperdimensionNeptunia

--- generate_list.py
+++ generate_list.py
@@ -1,20 +1,21 @@
 import json
 import os
+import pathlib
 
 out = []
 for root, dirs, files in os.walk('.'):
     for file in files:
         if file.endswith('.json'):
-            jpath = root + '\\' + file
-            with open(jpath, encoding='utf-8-sig') as f:
-                jdata = json.load(f)
-                if type(jdata) is dict and 'model' in jdata and jdata['model'].endswith('.moc'):
+            jPath = root + '\\' + file
+            with open(jPath, encoding='utf-8-sig') as f:
+                jData = json.load(f)
+                if type(jData) is dict and 'model' in jData and jData['model'].endswith('.moc'):
                     # n = file.replace('model', '').replace('json', '').strip('.').replace('.', '-')
                     # if len(n) == 0: n = root.strip('.')
-                    out.append(jpath)
+                    out.append(pathlib.Path(jPath).as_posix())
 
 
 with open('list.json', 'w', encoding='utf-8') as f:
-    json.dump(out, f)
+    json.dump(out, f, indent=4)
 
 print('Generated ' + str(len(out)) + ' items.')
